3d96cc60481a870f82f5b34c5d8c1b50d5ef38a7
ruddcas <ruddcas@gmail.com>
2020-02-07 14:27:32 -0500

    Add home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require.context('images', true, /\.(svg|png|jpg)$/igm);
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+//const images = require.context('../images', true, /\.(svg|png|jpg)$/igm);
+//const images = require.context('../images', true);
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-import '../images/palms.png';
+//const images = require.context('../images/palms.png', true);
+//const imagePath = (palms.png) => images(palms.png, true);
+//import '../images/palms.png';
+//import '../images/palms.png';//
+import '../stylesheets/application.css.scss';
+import './custom.js';
+import palms from '../images/palms.png'
